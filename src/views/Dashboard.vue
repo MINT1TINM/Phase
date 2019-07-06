df70b7ee56d0adf2bcd3374fa358a9f512bd152d
MINT <mint@insdim.com>
2019-07-06 17:06:47 +0800

    init

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>PHASE</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
