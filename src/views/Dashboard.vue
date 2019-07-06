b222c55ce76a2719e5f26c8ee138aee94e90b685
MINT <mint@insdim.com>
2019-07-06 17:18:18 +0800

    navbar

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,14 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>PHASE</span>
-      </v-toolbar-title>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <span class="font-weight-black navbar-logo ml-1">PHASE</span>
+      <v-spacer></v-spacer>
+      <v-avatar color="primary" size="36">
+        <span class="white--text">M</span>
+      </v-avatar>
     </v-app-bar>
 
     <v-content>
