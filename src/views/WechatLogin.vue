3882e61bbb1d888950b7c84a33bb33d1b0127ad8
MINT <mint@insdim.com>
2019-07-19 15:02:18 +0800

    wechat login redirect

--- src/views/WechatLogin.vue
+++ src/views/WechatLogin.vue
@@ -1,13 +1,15 @@
 <template></template>
 
 <script>
+import wechatService from "../service/WechatService";
 export default {
   data() {
     return {};
   },
   methods: {
     async getAccessToken() {
-      
+      let code = this.$route.params.code;
+      await wechatService.getAccessToken(code);
     }
   },
   async mounted() {
