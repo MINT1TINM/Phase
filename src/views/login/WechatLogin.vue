73e9b3654866972834d3ae7af0cf69afd644e4e7
MINT <mint@insdim.com>
2019-07-23 14:16:30 +0800

    fix wechat login

--- src/views/login/WechatLogin.vue
+++ src/views/login/WechatLogin.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-overlay :value="autoLoginProgress" :absolute="true" opacity="1" color="primary">
-    <v-card flat color="transparent">
-      <v-container>
-        <v-layout justify-center>
-          <v-progress-circular indeterminate size="64"></v-progress-circular>
-        </v-layout>
-      </v-container>
-      <v-card-text class="text-xs-center font-weight-black">
-        <strong>Phase</strong> 正在加载数据
-      </v-card-text>
-    </v-card>
-  </v-overlay>
+  <v-app>
+    <v-overlay :value="autoLoginProgress" :absolute="true" opacity="1" color="primary">
+      <v-card flat color="transparent">
+        <v-container>
+          <v-layout justify-center>
+            <v-progress-circular indeterminate size="64"></v-progress-circular>
+          </v-layout>
+        </v-container>
+        <v-card-text class="text-xs-center font-weight-black">
+          <strong>Phase</strong> 正在加载数据
+        </v-card-text>
+      </v-card>
+    </v-overlay>
+  </v-app>
 </template>
 
 <script>
